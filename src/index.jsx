4f2b359f7b7dbad617505bac935c1842b118ea3c
serhii_dotsyk <dosyk11@gmail.com>
2018-12-31 21:39:14 +0200

    fix errors with 'render'

--- src/index.jsx
+++ src/index.jsx
@@ -1,9 +1,9 @@
-import ReactDOM from 'react-dom';
+import { render } from 'react-dom';
 import React from 'react';
 import MainApp from './app/components/MainApp';
 import 'bootstrap/dist/css/bootstrap.css';
 
-ReactDOM.render(
+render(
   <MainApp />,
   document.getElementById('root')
 );
