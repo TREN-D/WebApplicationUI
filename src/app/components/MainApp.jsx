b6efe7c671bbe0de71dc97be2305766d77a307c1
Andriy Slesarchuk <slesarchukandr@gmail.com>
2018-12-31 20:53:11 +0200

    Create skeleton

---
+++ src/app/components/MainApp.jsx
@@ -0,0 +1,5 @@
+import { Component } from 'react';
+
+export default class MainApp extends Component {
+
+}
---
+++ src/index.jsx
@@ -0,0 +1,9 @@
+import ReactDOM from 'react-dom';
+import React from 'react';
+import MainApp from './app/components/MainApp';
+import 'bootstrap/dist/css/bootstrap.css';
+
+ReactDOM.render(
+  <MainApp />,
+  document.getElementById('root')
+);
