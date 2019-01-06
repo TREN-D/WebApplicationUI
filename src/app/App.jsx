f60ae295348942db2523b2a9de25f3497dfb1ee0
Andriy Slesarchuk <slesarchukandr@gmail.com>
2019-01-06 02:10:10 +0200

    Configure environment

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -1,7 +1,6 @@
-
 import React, { Component } from 'react';
 
-export default class MainApp extends Component {
+export default class App extends Component {
   render() {
     return (<div>Hello</div>);
   }
